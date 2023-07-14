54eafbff503dc1a8d9bc5390e78045b9296d68f7
4KENSENS <kerem1eren@gmail.com>
2023-07-14 12:32:17 +0300

    feat: create graphic

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,25 @@ import org.jfree.data.xy.DefaultXYDataset;
 
 public class Main {
     public static void main(String[] args) {
-        String imagePath = "Karton2.jpg"; // Analiz etmek istediğiniz resmin dosya yolu
+        String imagePath = "Karton.jpg"; // Analiz etmek istediğiniz resmin dosya yolu
 
         try {
             BufferedImage image = ImageIO.read(new File(imagePath));
             int width = image.getWidth();
             int height = image.getHeight();
 
-            double[][] data = new double[2][width];
+            double[][] data = new double[2][height];
 
-            // Her bir yatay pikseli dolaşarak dikeydeki piksellerin gri tonlarını topla ve veri setine ekle
-            for (int x = 0; x < width; x++) {
+            // Her bir dikey pikseli dolaşarak yataydaki piksellerin gri tonlarını topla ve veri setine ekle
+            for (int y = 0; y < height; y++) {
                 int graySum = 0;
-                for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
                     Color pixelColor = new Color(image.getRGB(x, y));
                     int grayScale = (pixelColor.getRed() + pixelColor.getGreen() + pixelColor.getBlue()) / 3;
                     graySum += grayScale;
                 }
-                data[0][x] = x + 1; // Yatay piksel sayısı
-                data[1][x] = graySum; // Dikeydeki gri ton toplamı
+                data[0][y] = y + 1; // Dikey piksel sayısı
+                data[1][y] = graySum; // Yataydaki gri ton toplamı
             }
 
             DefaultXYDataset dataset = new DefaultXYDataset();
@@ -36,26 +36,14 @@ public class Main {
 
             JFreeChart chart = ChartFactory.createXYLineChart(
                     "Gri Ton Toplamları",
-                    "Yatay Piksel Sayısı",
-                    "Dikeydeki Gri Ton Toplamı",
+                    "Dikey Piksel Sayısı",
+                    "Yataydaki Gri Ton Toplamı",
                     dataset
             );
 
             ChartFrame frame = new ChartFrame("Gri Ton Analizi", chart);
             frame.pack();
             frame.setVisible(true);
-
-            // Tepe noktalarını hesapla
-            int peakCount = 0;
-            for (int i = 1; i < width - 1; i++) {
-                double prevValue = data[1][i - 1];
-                double currentValue = data[1][i];
-                double nextValue = data[1][i + 1];
-                if (currentValue > prevValue && currentValue > nextValue) {
-                    peakCount++;
-                }
-            }
-            System.out.println("Tepe Noktalarının Sayısı: " + peakCount);
         } catch (IOException e) {
             e.printStackTrace();
         }
