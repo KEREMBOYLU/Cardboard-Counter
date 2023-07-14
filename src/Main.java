4463a17c19e69e723c04f6e59057411ad2b04fd1
4KENSENS <kerem1eren@gmail.com>
2023-07-14 11:34:22 +0300

    feat: stage 2

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,29 @@
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.statistics.HistogramDataset;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // 1. Görüntüyü yükle
-            File file = new File("Karton.jpg");
-            BufferedImage image = ImageIO.read(file);
+        String imagePath = "Kartonkare.jpg"; // Analiz etmek istediğiniz resmin dosya yolu
 
-            // 2. Genişlik ve yüksekliği al
+        try {
+            BufferedImage image = ImageIO.read(new File(imagePath));
             int width = image.getWidth();
             int height = image.getHeight();
 
-            System.out.println(width +"\n");
-            System.out.println(width +"\n");
-
-            // 3. Histogramı hesapla
-            HistogramDataset dataset = new HistogramDataset();
-            double[] values = new double[width * height];
-            int index = 0;
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int rgb = image.getRGB(x,y);
-                    int gray = (rgb >> 16) & 0xFF; // Gri ton değeri
-                    values[index] = gray;
-                    index++;
+            // Her bir yatay pikseli dolaşarak dikeydeki piksellerin gri tonlarını topla ve yazdır
+            for (int x = 0; x < width; x++) {
+                int graySum = 0;
+                for (int y = 0; y < height; y++) {
+                    Color pixelColor = new Color(image.getRGB(x, y));
+                    int grayScale = (pixelColor.getRed() + pixelColor.getGreen() + pixelColor.getBlue()) / 3;
+                    graySum += grayScale;
                 }
+                System.out.println("Yatay pixel sayısı: " + (x + 1) + ", dikeydeki gri ton toplamı: " + graySum);
             }
-            dataset.addSeries("Histogram", values, 256);
-
-            // 4. Histogramı grafik olarak oluştur ve görüntüle
-            JFreeChart chart = ChartFactory.createHistogram("Histogram", "Gri Ton Değeri", "Piksel Sayısı", dataset);
-            JFrame frame = new JFrame("Histogram");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(new ChartPanel(chart));
-            frame.pack();
-            frame.setVisible(true);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
