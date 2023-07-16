7d803a3eeb38e35568cbd516848fa49380eb96e3
4KENSENS <kerem1eren@gmail.com>
2023-07-17 00:58:00 +0300

    feat: almost done

--- src/Main.java
+++ src/Main.java
@@ -22,17 +22,6 @@ public class Main {
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             String imagePath = selectedFile.getAbsolutePath();
-            String fileName = selectedFile.getName();
-
-            int threshold ;
-
-            if (fileName.equals("karton2.jpg")) {
-                threshold = 73000;
-            }
-            else {
-                threshold = 208000;
-            }
-
 
             try {
                 BufferedImage image = ImageIO.read(new File(imagePath));
@@ -67,20 +56,86 @@ public class Main {
                 frame.pack();
                 frame.setVisible(true);
 
-                // Calculate the number of peaks
-                int peakCount = 0;
-                for (int y = 1; y < height - 1; y++) {
-                    double prevValue = data[1][y - 1];
-                    double currentValue = data[1][y];
-                    double nextValue = data[1][y + 1];
-                    if (currentValue > prevValue && currentValue > nextValue && currentValue > threshold) {
-                        peakCount++;
-                    }
-                }
-                System.out.println("Number of Peaks above " + threshold + ": " + peakCount);
+                // Apply smoothing to the data
+                int windowSize = 45; // Size of the window for smoothing
+
+                double[][] smoothedData = smoothData(data, windowSize);
+
+                DefaultXYDataset smoothedDataset = new DefaultXYDataset();
+                smoothedDataset.addSeries("Smoothed Grayscale Sums", smoothedData);
+
+                JFreeChart smoothedChart = ChartFactory.createXYLineChart(
+                        "Smoothed Grayscale Sums",
+                        "Vertical Pixel Count",
+                        "Sum of Grayscale Values in the Horizontal Direction",
+                        smoothedDataset
+                );
+
+                ChartFrame smoothedFrame = new ChartFrame("Smoothed Grayscale Analysis", smoothedChart);
+                smoothedFrame.pack();
+                smoothedFrame.setVisible(true);
+
+                // Calculate the number of peaks in the smoothed data
+                int peakCount = countPeaks(smoothedData);
+
+                System.out.println("Number of Peaks: " + peakCount);
+
+                // Show the smoothed data chart
+                ChartFrame smoothedDataChartFrame = new ChartFrame("Smoothed Data Analysis", createChart(smoothedData));
+                smoothedDataChartFrame.pack();
+                smoothedDataChartFrame.setVisible(true);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static double[][] smoothData(double[][] data, int windowSize) {
+        int height = data[0].length;
+        int smoothedHeight = height - windowSize + 1;
+
+        double[][] smoothedData = new double[2][smoothedHeight];
+
+        for (int i = 0; i < smoothedHeight; i++) {
+            int sum = 0;
+            for (int j = i; j < i + windowSize; j++) {
+                sum += data[1][j];
+            }
+            smoothedData[0][i] = data[0][i + windowSize / 2]; // Use the middle point of the window
+            smoothedData[1][i] = sum / windowSize; // Average of the values in the window
+        }
+
+        return smoothedData;
+    }
+
+    private static int countPeaks(double[][] data) {
+        int peakCount = 0;
+        int height = data[0].length;
+
+        for (int i = 1; i < height - 1; i++) {
+            double prevValue = data[1][i - 1];
+            double currentValue = data[1][i];
+            double nextValue = data[1][i + 1];
+            if (currentValue > prevValue && currentValue > nextValue) {
+                peakCount++;
+            }
+        }
+
+        return peakCount;
+    }
+
+    private static JFreeChart createChart(double[][] data) {
+        DefaultXYDataset dataset = new DefaultXYDataset();
+        dataset.addSeries("Smoothed Grayscale Sums", data);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Smoothed Data Analysis",
+                "Vertical Pixel Count",
+                "Sum of Grayscale Values in the Horizontal Direction",
+                dataset
+        );
+
+        return chart;
     }
 }
